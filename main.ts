fc1708345a7ded5ae53aafdc8168bac508370ad1
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-12-17 19:36:21 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`space-ship-lvl1`, DeathStar, 100, 0)
+    projectile.x += 20
+    projectile.y += -3
+})
+let projectile: Sprite = null
+let DeathStar: Sprite = null
+DeathStar = sprites.create(assets.image`vesmirna-lod`, SpriteKind.Player)
+controller.moveSprite(DeathStar, 0, 100)
+DeathStar.setStayInScreen(true)
+DeathStar.setPosition(0, 60)
